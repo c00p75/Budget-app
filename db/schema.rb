a65811588aee273e03f50debd469712a7f0981ad
c00p75 <georgecoopmsapenda@gmail.com>
2023-07-03 12:50:51 +0200

    Add validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_03_103804) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_03_104700) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_03_103804) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "category_purchases", force: :cascade do |t|
