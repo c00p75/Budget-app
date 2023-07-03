15b16d803dac9acac418d1f77cec46173971c679
c00p75 <georgecoopmsapenda@gmail.com>
2023-07-03 22:08:03 +0200

    Add check_authenticationto application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "splash#index"
+  
+  root "purchases#index"
   resources :category_purchases
   resources :purchases
   resources :categories
   resources :users
+
+  get 'budget_wiz', to: 'splash#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
