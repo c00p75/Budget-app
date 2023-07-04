557f6f6b49df04af104acacdbe2a8a694344595c
c00p75 <georgecoopmsapenda@gmail.com>
2023-07-04 11:36:56 +0200

    remove confirmable from sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root "purchases#index"
+  root "categories#index"
   resources :category_purchases
   resources :purchases
   resources :categories
