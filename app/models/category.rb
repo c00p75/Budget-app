2cb08f31f315d94ec1df8faff70d266050bdf9c2
c00p75 <georgecoopmsapenda@gmail.com>
2023-07-05 10:17:29 +0200

    Capitalize category entries

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,12 @@ class Category < ApplicationRecord
   has_and_belongs_to_many :purchases
 
   validates :name, presence: true, uniqueness: true
+
+  before_create :capitalize_name
+
+  private
+
+  def capitalize_name
+    self.name = name.titleize
+  end
 end
